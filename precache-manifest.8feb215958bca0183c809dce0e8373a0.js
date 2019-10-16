3587304815b7b995d531fb869f1d730d416282e0
Daniel Merkl <dan.merkl.90@gmail.com>
2019-10-16 13:51:22 -0500

    Updates

---
+++ precache-manifest.8feb215958bca0183c809dce0e8373a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dbdf0fef69771e2e3b4c6bc8c5b76b",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "e42dd4421c85a1b26a6e",
+    "url": "/click-game/static/css/main.d9055f04.chunk.css"
+  },
+  {
+    "revision": "2c6fe41e04e44121834a",
+    "url": "/click-game/static/js/2.ca6dbceb.chunk.js"
+  },
+  {
+    "revision": "e42dd4421c85a1b26a6e",
+    "url": "/click-game/static/js/main.489f8f6f.chunk.js"
+  },
+  {
+    "revision": "283968ef8abcfc56e186",
+    "url": "/click-game/static/js/runtime-main.e645ff9b.js"
+  }
+]);
